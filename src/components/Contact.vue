a40782bebfa6c30cfe2f1ab0fc5e836bf1cacdd3
Nilesh <mishranileshkumar6@gmail.com>
2022-01-07 01:35:16 +0530

    portfolio

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,99 +1,102 @@
 <template>
-  <div class="about pa-4">
-    <v-card  elevation="0" min-height="700" >
-      <v-card-text class="pa-6 about">
-          <v-row class="justify-center">
-              <v-form ref="form"
-    v-model="valid"
-    lazy-validation
-              style="width:500px">
-    <v-text-field
-      v-model="name"
-      :counter="20"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
+    <div class="container">
+        <form @submit.prevent="sendEmail">
+          <label>Name</label>
+          <input
+            type="text"
+            v-model="name"
+            name="name"
+            placeholder="Your Name"
+          >
+          <label>Email</label>
+          <input
+            type="email"
+            v-model="email"
+            name="email"
+            placeholder="Your Email"
+            >
+          <label>Message</label>
+          <textarea
+            name="message"
+            v-model="message"
+            cols="30" rows="5"
+            placeholder="Message">
+          </textarea>
 
-    <v-btn
-      class="mr-4"
-      :disabled="!valid"
-      @click="submit"
-      color="primary"
-    >
-      submit
-    </v-btn>
-    <v-btn text @click="reset">
-      clear
-    </v-btn>
-  </v-form>
-          </v-row>
-      </v-card-text>
-    </v-card>
-  </div>
+          <input type="submit" value="Send">
+        </form>
+    </div>
 </template>
+
 <script>
+import emailjs from 'emailjs-com'
+
 export default {
-  name: 'contact',
+  name: 'ContactUs',
   data () {
     return {
-      valid: false,
       name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 20) || 'Name must be less than 10 characters'
-      ],
       email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-      ],
-      select: null,
-      items: [
-        'Student',
-        'Contributor',
-        'Client'
-      ],
-      checkbox: false
+      message: ''
     }
   },
   methods: {
-    validate () {
-      this.$refs.form.validate()
-    },
-    reset () {
-      this.$refs.form.reset()
-    },
-    resetValidation () {
-      this.$refs.form.resetValidation()
+    sendEmail (e) {
+      try {
+        emailjs.sendForm('service_wbd6p8h', 'template_aen56s8', e.target, 'user_FgLHgYLQSrs6vI0oMKe7n', {
+          name: this.name,
+          email: this.email,
+          message: this.meessage
+        })
+        console.log('it works!!!')
+      } catch (error) {
+        console.log({ error })
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
     }
   }
 }
 </script>
+
 <style scoped>
-.about {
-    font-size: 20px;
-    font-family: monospace;
-    color: darkmagenta !important;
-    line-height: 43px;
+* {box-sizing: border-box;}
+
+label {
+  float: left;
+}
+input[type=text], [type=email], textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #b8c7b9;
+}
+
+.container {
+  display: block;
+  margin:auto;
+  text-align: center;
+  border-radius: 5px;
+  padding: 20px;
+  width: 50%;
 }
 </style>
